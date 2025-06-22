5342a0facdfbfe9164c44193c9dfafb091511282
Hina Shafique <hinashafique4159@gmail.com>
2025-06-22 19:58:21 +0500

    make  models with artisan command alonge with --all like migration, seeding etc files

---
+++ app/Http/Controllers/FreelancerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\freelancer;
+use App\Http\Requests\StorefreelancerRequest;
+use App\Http\Requests\UpdatefreelancerRequest;
+
+class FreelancerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorefreelancerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(freelancer $freelancer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(freelancer $freelancer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatefreelancerRequest $request, freelancer $freelancer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(freelancer $freelancer)
+    {
+        //
+    }
+}
---
+++ app/Models/freelancer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class freelancer extends Model
+{
+    /** @use HasFactory<\Database\Factories\FreelancerFactory> */
+    use HasFactory;
+}
